4e119d888823809be056c2e81a5bd6874a78bca1
AhmadKater <ahmed.said3439@yahoo.com>
2021-07-13 23:53:02 +0200

    using try-catch-finally block

--- C#Exceptions/HandlingExceptions/Program.cs
+++ C#Exceptions/HandlingExceptions/Program.cs
@@ -9,7 +9,7 @@ namespace HandlingExceptions
             var c = new Calculator();
             
             System.Console.WriteLine("Enter 1st num : ");
-            var n1  = int.Parse(Console.ReadLine());
+            int n1  = int.Parse(Console.ReadLine()) ;
             
             System.Console.WriteLine(" ");
             
@@ -17,20 +17,34 @@ namespace HandlingExceptions
             var n2 = int.Parse(Console.ReadLine());
 
             System.Console.WriteLine(" ");
-/*
+
             System.Console.WriteLine("Enter Operation : ");
 
             var o = Console.ReadLine() ;
 
             System.Console.WriteLine(" ");
-*/
+
             try
             {
-                System.Console.WriteLine(c.Calculate(n1,n2,null));
+                System.Console.WriteLine(c.Calculate(n1,n2,o));
+            }
+            catch (ArgumentNullException e)
+            {
+                
+                System.Console.WriteLine($"Operation was not provided, {e}");
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                System.Console.WriteLine($"Operation is not supported, {e}");
             }
             catch (Exception e)
             {
-                System.Console.WriteLine("Something Went Wrong : " + e.Message + " .");                
+                System.Console.WriteLine("Something Went Wrong : " + e + " .");                
+            }
+            finally
+            {
+                System.Console.WriteLine("Finally Block is always executed");
             }
 
         }
